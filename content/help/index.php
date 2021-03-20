db4542741ae68c98a8a6bbf68833a646121b9cc9
Few Peeranat <sharpkung4826@gmail.com>
2021-03-20 20:05:54 +0700

    Init Add index.php [/content/help]

---
+++ content/help/index.php
@@ -0,0 +1,30 @@
+<?php
+namespace content\help;
+
+use template\TemplateHandle;
+
+TemplateHandle::printTitle("หน้าช่วยเหลือ");
+TemplateHandle::printTitleTopic("Q & A", "ตอบคำถามเรื่องการใช้งานเว็บไซต์");
+?>
+<section class="section section-lg pt-0">
+	<div class="container mt-n8 mt-lg-n12 z-2">
+		<div class="row justify-content-center">
+			<div class="col">
+				<div class="card border-light p-4 p-lg-5">
+					<div class="col-12 text-center mb-2">
+                        <h2>— หน้าช่วยเหลือ —</h2>
+                    </div>
+                    <p class="note note-warning">
+                      <strong>Note :</strong> ในอนาคตอาจจะเพิ่มคำถามหรือคำตอบไว้เพิ่มเติม เพราะเว็บนี้พัฒนาโดยคนขยันแห่งชาติและหน้าตาดี (ノ-_-)ノ
+                    </p>
+                    <br />
+                    <span><b>Q: </b> ทำไมต้องมีเว็บไซต์นี้</span>
+                    <span><b>A: </b> เพราะว่าให้โรงเรียนมหาวชิราวุธ จังหวัดสงขลามีเว็บไซต์สารสนเทศในการจัดการระบบให้ดูทันสมัยมากขึ้น</span>
+                    <br />
+                    <span><b>Q: </b> คนทำเว็บไซต์นี้ได้เงินเดือนมั้ย</span>
+                    <span><b>A: </b> ไม่ได้จ้า เพราะว่าเป็นนักเรียนมหาวชิราวุธ แต่คาดว่าคงได้รับนักเรียนดีเด่น เลิศประเสริฐสี มณี 7 ＼(≧▽≦)／</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
